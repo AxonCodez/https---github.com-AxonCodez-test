0bd0f90ec1f6d5ae38eea694ca125698216cf593
Akshay K <axongfx.help@gmail.com>
2025-09-21 18:41:17 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,6 +6,7 @@ const firebaseConfig = {
   "appId": "1:159639894417:web:19eefb74aa8e4b0a96c230",
   "apiKey": "AIzaSyAWJ_WY1qGpazhx6TOG3nYr2C0ldNY2cN0",
   "authDomain": "studio-6597146310-3402d.firebaseapp.com",
+  "measurementId": "",
   "messagingSenderId": "159639894417"
 };
 
